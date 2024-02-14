75cc62a7a462ef7e50aaa464b85eda94f1640669
Fleserig73 <88104726+Fleserig73@users.noreply.github.com>
2024-02-14 12:08:56 +0100

    saving

---
+++ README.md
@@ -0,0 +1,37 @@
+# Homen
+
+This chrome extension will display a random quote every time you open a new tab.
+
+<img src="homen.png">
+
+## Installation
+
+1. Clone this repository or download the ZIP file
+
+```shell
+git clone https://github.com/Fleserig73/Homen.git
+```
+
+2. Open Google Chrome and go to `chrome://extensions/` in your address bar.
+
+3. Enable the "Developer mode" toggle on the top right corner of the page.
+
+4. Click on the "Load unpacked" button and select the cloned/downloaded folder.
+
+5. The extension should now be installed and ready to use.
+
+## Usage
+
+1. Open a new tab in Google Chrome.
+
+2. You will see a random quote displayed along with the current weather information of your location.
+
+## Customization
+
+Feel free to customize the extension by modifying:
+
+- The styling in `style.css` for layout and appearance changes.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- script.js
+++ script.js
@@ -14,11 +14,43 @@ function showTime() {
 }
 showTime();
 
-document.getElementById('bg').style.backgroundImage = 'url(https://picsum.photos/1920/1080)'
+onStart();
 
-fetch("https://quotable.io/random/")
-    .then(res => res.json())
-    .then(data => {
-        document.querySelector('#quote').innerHTML = data.content;
-        document.querySelector('#author').innerHTML = data.author;
-    })
+function onStart(){
+    chrome.storage.local.get("date", function(lastDate){
+        const minute = 1000 * 60;
+        const d = new Date();
+        let now = Math.round(d.getTime() / minute);
+        console.log(lastDate.date)
+        if (lastDate.date == undefined){
+            lastDate.date = 0
+        }
+        if (now - lastDate.date > 10){
+            chrome.storage.local.set({ "date": now });
+            var url = "https://source.unsplash.com/random/1920x1080"
+            fetch(url).then(data=>{
+                chrome.storage.local.set({ "pictureurl": data.url });
+                console.log(data)
+            });
+            document.getElementById('bg').style.backgroundImage = `url(${url})`
+            fetch("https://quotable.io/random/")
+                .then(res => res.json())
+                .then(data => {
+                    document.querySelector('#quote').innerHTML = data.content;
+                    document.querySelector('#author').innerHTML = data.author;
+                    chrome.storage.local.set({ "phrase": [data.content, data.author] });
+                })
+        } 
+        else {
+            chrome.storage.local.get("pictureurl", function(url){
+                document.getElementById('bg').style.backgroundImage = `url(${url.pictureurl})`
+            });
+            chrome.storage.local.get("phrase", function(url){
+                document.querySelector('#quote').innerHTML = url.phrase[0];
+                document.querySelector('#author').innerHTML = url.phrase[1];
+            });
+        }
+    });
+}
+
+    
